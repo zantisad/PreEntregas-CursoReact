95c8b6fb68eafb3879adff1d4b65243050a74b5c
santiago <santigonzalez20032003@gmail.com>
2024-08-20 10:48:03 -0300

    Arreglo del header y agregado de productos nuevos

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import "./Portada.css";
+
+import React from "react";
 import { NavLink, Link } from "react-router-dom";
 
 const Portada = () => {
-    return (
-        <div className='portada'>
+  return (
+    <div className="portada">
+      <div className="portada-contenedor">
+        <img
+          className="portada-imagen"
+          src="https://i.pinimg.com/originals/a4/29/2f/a4292fb06e15c8f288d3e3896665d52b.jpg"
+          alt="Portada Pagina"
+        />
+      </div>
+      <NavLink>
+        <span className="portada-texto">VER PRODUCTO</span>
+      </NavLink>
+    </div>
+  );
+};
 
-            <div className="portada-contenedor">
-                <div className="portada-imagen">
-                    <img src="https://i.pinimg.com/originals/a4/29/2f/a4292fb06e15c8f288d3e3896665d52b.jpg" alt="Portada Pagina" />
-                </div>
-                <div className="portada-texto">
-                    <NavLink className=''>IR AL PRODUCTO</NavLink>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Portada
+export default Portada;
